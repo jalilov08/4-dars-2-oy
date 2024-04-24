eeee23e475b4c397698ca3006e78433c74db4cfe
jalilov08 <jalilovnozimjon054@gmail.com>
2024-04-25 00:43:51 +0500

    Homework

---
+++ app.js
@@ -0,0 +1,105 @@
+//1-misol
+// let k = 8;
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+// console.log(k);
+// }
+
+
+//2-misol
+// let a = 6;
+// let b = 9;
+// let res = 0;
+// for (let i = a; i <= b; i++) {
+// console.log(i);
+// res++;
+// }
+// console.log("Jami:" res);
+
+
+//3-misol
+// let a = 2;
+// let b = 10;
+// let res = 0;
+// for (let i = b - 1; i > a; i--) {
+// console.log(i);
+// res++;
+// }
+// console.log("Jami:", res);
+
+
+//4-misol
+// let a = 6;
+// for (let i = 1; i <= 10; i++) {
+// let res = i * a;
+// console.log();
+// }
+
+
+//5-misol
+// let a = 0.2;
+// for (let i = 0.1; i <= 1; i += 0.1) {
+// let res = i * a;
+// console.log(res);
+// }
+
+
+//6-misol
+// let a = 2;
+// for (let i = 1.2; i <= 2; i += 0.2) {
+// let res = i * a;
+// console.log(res);
+// }
+
+
+//7-misol
+// let a = 2;
+// let b = 12;
+// let res = 0;
+// for (let i = a; i <= b; i++) {
+// res += i;
+// }
+// return res;
+// console.log(res);
+
+
+//8-misol
+// let a = 3;
+// let b = 10;
+// let res = 0;
+// for (let i = a; i <= b; i++) {
+// res *= i;
+// }
+// return res;
+// console.log(res);
+
+
+//9-misol
+// let a = 4;
+// let b = 8;
+// let res = 0;
+// for (let i = a; i <= b; i++) {
+// res += i * i;
+// }
+// return res;
+// console.log(res);
+
+
+//10-misol
+// let n = 4;
+// let S = 0;
+// for (let i = 1; i <= n; i++) {
+// S += 1 / i;
+// }
+// return S;
+// console.log(S);
+
+
+//11-misol
+// let n = 4;
+// let S = 1;
+// for (let i = 1; i <= 0.1; i += i) {
+// S *= i
+// }
+// return S;
+// console.log(S);
